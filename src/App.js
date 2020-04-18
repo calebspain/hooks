4873d17e76a8d082b8269ddf9a56cd3adb28e823
Caleb Spain <crspain@uh.edu>
2020-04-18 13:37:26 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+let born = false;
 function App() {
+  const [growth, setGrowth] = useState(0);
+  const [nirvana, setNirvana] = useState(false);
+
+  useEffect(() => {
+    if (born) {
+      document.title = 'Nirvana Attained';
+    }
+  }, [nirvana]);
+
+  useEffect(() => {
+    console.log('I am born');
+  }, []);
+
+  useEffect(() => {
+    if (born) {
+      console.log('make mistake and learn');
+    } else {
+      born = true;
+    }
+
+    if (growth > 70) {
+      setNirvana(true);
+    }
+
+    return () => {
+      console.log('cleanup after mistakes');
+    }
+  }, [growth]);
+
+  const growHandle = () => {
+    setGrowth(growth + 10);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Use Effect</h2>
+        <h3>Growth: {growth}</h3>
+        <button onClick={growHandle}>Learn and Grow</button>
       </header>
     </div>
   );
